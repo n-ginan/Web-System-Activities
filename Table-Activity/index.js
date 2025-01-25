c10b13c27911d0f1dbbdd1acbe2d354fbbcb1986
papsykle <toramgarcia@gmail.com>
2025-01-25 17:37:56 +0800

    UI finalized

--- Table-Activity/index.js
+++ Table-Activity/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const inputForm = document.getElementById("inputForm")
+    const calculateBtn = document.getElementById("calculateBtn")
     
+    calculateBtn.addEventListener("click", () => {
+        const formData = Object.fromEntries(new FormData(inputForm).entries())
+        var firstNumber = Number(formData["first_number"])
+        var secondNumber = Number(formData["second_number"])
+        
+    })
 })
