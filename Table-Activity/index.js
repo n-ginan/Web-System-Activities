2055c1a0f6940f4563e69c2125c1bfbed3f6f5ba
papsykle <toramgarcia@gmail.com>
2025-01-25 18:04:57 +0800

    created input handling and some animations

--- Table-Activity/index.js
+++ Table-Activity/index.js
@@ -6,6 +6,18 @@ document.addEventListener("DOMContentLoaded", () => {
         const formData = Object.fromEntries(new FormData(inputForm).entries())
         var firstNumber = Number(formData["first_number"])
         var secondNumber = Number(formData["second_number"])
+        var inputCondition = (firstNumber < 1 || firstNumber > 5) 
+                             || (secondNumber < 1 || secondNumber > 5) 
+                             || (isNaN(firstNumber) || isNaN(secondNumber))
+
+        if (!inputCondition) {
+            
+        } else {
+            const inputErrorLabel = document.getElementsByTagName("p")
+            inputErrorLabel[0].style.animation = "none"
+            void inputErrorLabel[0].offsetWidth
+            inputErrorLabel[0].style.animation = "tilt-shake 0.4s"
+        }
         
     })
 })
