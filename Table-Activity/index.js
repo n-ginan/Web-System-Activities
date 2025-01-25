3e3fdb33b6bbbeff3488141a51a49a44dc0a6fcf
papsykle <toramgarcia@gmail.com>
2025-01-25 20:01:01 +0800

    activity finished

--- Table-Activity/index.js
+++ Table-Activity/index.js
@@ -1,17 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
     const inputForm = document.getElementById("inputForm")
     const calculateBtn = document.getElementById("calculateBtn")
-    
+    var currentTD = 0
+
     calculateBtn.addEventListener("click", () => {
         const formData = Object.fromEntries(new FormData(inputForm).entries())
         var firstNumber = Number(formData["first_number"])
         var secondNumber = Number(formData["second_number"])
-        var inputCondition = (firstNumber < 1 || firstNumber > 5) 
+        const inputCondition = (firstNumber < 1 || firstNumber > 5) 
                              || (secondNumber < 1 || secondNumber > 5) 
                              || (isNaN(firstNumber) || isNaN(secondNumber))
 
         if (!inputCondition) {
-            
+            const tableData = document.getElementsByTagName("td")
+            if (currentTD % 6 != 0) {
+                tableData[currentTD].style.background = "rgb(254, 234, 208)"
+                tableData[currentTD].style.color = "#975220"
+            }
+            Array.from(tableData).forEach((td, tdIndex) => {
+                if (tdIndex % 6 == 0 && secondNumber == parseInt(td.textContent)) {
+                    currentTD = tdIndex + firstNumber
+                    tableData[currentTD].style.background = "#975220"
+                    tableData[currentTD].style.color = "rgb(254, 234, 208)"
+                }
+            })
         } else {
             const inputErrorLabel = document.getElementsByTagName("p")
             inputErrorLabel[0].style.animation = "none"
